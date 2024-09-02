a13b348541a3ac266d1dbb2f430c9110a8784f7d
Giovani <giovanigiggio13@gmail.com>
2024-09-02 10:41:14 -0300

    in principio

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import CustomersReviews from "./CustomersReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Nav from "./Nav";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+    CustomersReviews,
+    Footer,
+    Hero,
+    Nav,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+}
---
+++ constants/index.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+    {href: "'home", label:"Home"},
+    { href: "#about-us", label:"About-us"},
+    {href: "products", label:"Products"},
+    {href: "#contact-us", label:"Contact us"},
+]
